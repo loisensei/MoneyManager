24cae2e139c1167f806e684e72cbdd73832c4c6d
loisensei <loitran299@gmail.com>
2021-11-29 21:45:34 +0700

    final version

--- src/main/java/com/oop/moneymanager/view/AddAccountPopup.java
+++ src/main/java/com/oop/moneymanager/view/AddAccountPopup.java
@@ -19,7 +19,6 @@ import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
